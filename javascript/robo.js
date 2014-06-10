77810324e3229785ef5a11142882f7bc07132e2a
Ben Rudi <brudi07@gmail.com>
2014-06-10 08:17:08 -0500

    Adding to the objects
    Fleshed out the robot object a bit. Added part and ability. Moved the
    objects to the bottom of the file.

--- javascript/robo.js
+++ javascript/robo.js
@@ -1,42 +1,28 @@
 // create the game canvas/world
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 
-// create the player
-function Player(game) {
-  this.game = game;
-  this.health = 20;
-
-  this.getHealth = function() {
-    return this.health;
-  }
-
-  this.setHealth = function(health) {
-    return this.health = health;
-  }
-}
-
-// main menu
+// main menu state
 var main_menu = {
 
     preload: function() { 
 
-        // create battle button
-        main_menu.game.load.image('battle_button', 'assets/robo/battle.png');
+        // create battle_menu button
+        main_menu.game.load.image('battle_menu_button', 'assets/robo/battle_menu.png');
 
     },
 
     create: function() { 
 
-        // add the battle button to the menu
-        main_menu.game.add.button(game.world.centerX - 75, game.world.centerY - 25, 'battle_button', this.goToBattle, this);
+        // add the battle_menu button to the menu
+        main_menu.game.add.button(game.world.centerX - 75, game.world.centerY - 25, 'battle_menu_button', this.goTobattle_menu, this);
 
     },
 
-    goToBattle: function() {
+    goTobattle_menu: function() {
 
-        // goto battle
-        main_menu.game.state.add('battle', battle);  
-        main_menu.game.state.start('battle'); 
+        // goto battle_menu
+        main_menu.game.state.add('battle_menu', battle_menu);  
+        main_menu.game.state.start('battle_menu'); 
 
     },
 
@@ -47,40 +33,43 @@ var main_menu = {
     }
 };
 
-// battle state
-var battle = {
+// battle menu state
+var battle_menu = {
 
     preload: function() { 
 
         // background image
-        battle.stage.game.stage.backgroundColor = '#000000';
+        battle_menu.stage.game.stage.backgroundColor = '#000000';
 
         // buttons
-        battle.game.load.image('run_button', 'assets/robo/run.png');
+        battle_menu.game.load.image('run_button', 'assets/robo/run.png');
 
         // load images for robots
-        battle.game.load.image('robot', 'assets/robo/robot.png');  
+        battle_menu.game.load.image('player', 'assets/robo/robot.png');  
+        battle_menu.game.load.image('enemy', 'assets/robo/robot.png');  
 
     },
 
     create: function() { 
 
         // create robot on screen
-        battle.robot = this.game.add.sprite(100, 245, 'robot');
+        battle_menu.player = this.game.add.sprite(100, 245, 'player');
+        battle_menu.enemy = this.game.add.sprite(300, 145, 'enemy');
 
         // create health bar for player
         var style = { font: "30px Arial", fill: "#ffffff" };
-        battle.label_health = this.game.add.text(200, 245, player.health, style); 
+        battle_menu.label_name = this.game.add.text(175, 245, robot.name, style); 
+        battle_menu.label_health = this.game.add.text(200, 245, robot.Currenthealth, style); 
 
         // create buttons
-        battle.game.add.button(100, 345, 'run_button', this.runAway, this);
+        var runButton = battle_menu.game.add.button(100, 345, 'run_button', this.runAway, this);
 
     },
 
     updateHealth: function() {
 
         // update health
-        battle.label_health.content = player.health;  
+        battle_menu.label_health.content = player.currentHealth;  
 
     },
 
@@ -93,7 +82,98 @@ var battle = {
     }
 };
 
+// robot object
+function robot(name, health, physicalAttack, physicalDefense, specialAttack, specialDefense, speed) {
+    // name
+    this.name = name;
+    // health
+    this.maxHealth = health;
+    this.currentHealth = health;
+    // physical attack and defense
+    this.maxPhysicalAttack = physicalAttack;
+    this.currentPhysicalAttack = physicalAttack;
+    this.maxPhysicalDefense = physicalDefense;
+    this.currentPhysicalDefense = physicalDefense;
+    // special attack and defense
+    this.maxSpecialAttack = specialAttack;
+    this.currentSpecialAttack = specialAttack;
+    this.maxSpecialDefense = specialDefense;
+    this.currentSpecialDfense = specialDefense;
+    // speed
+    this.maxSpeed = speed;
+    this.currentSpeed = speed;
+
+    // getters and setters for attributes
+    this.getMaxHealth = function() {
+        return this.maxHealth;
+    }
+
+    this.seMaxHealth = function(maxHealth) {
+        return this.maxHealth = maxHealth;
+    }
+
+    this.getCurrentHealth = function() {
+        return this.currentHealth;
+    }
+
+    this.seCurrentHealth = function(currentHealth) {
+        return this.currentHealth = currentHealth;
+    }
+
+    // equip parts
+    this.equipPart = function() {
+
+    }
+
+    // check if parts equipped
+    this.isEquipped = function() {
+
+    }
+
+    // level up! +stats
+    this.levelUp = function() {
+
+    }
+}
+
+// part object
+function part(name, health, physicalAttack, physicalDefense, specialAttack, specialDefense, speed, ability) {
+    // name
+    this.name = name;
+    // health
+    this.health = health;
+    // physical attack and defense
+    this.physicalAttack = physicalAttack;
+    this.physicalDefense = physicalDefense;
+    // special attack and defense
+    this.specialAttack = specialAttack;
+    this.specialDefense = specialDefense;
+    // speed
+    this.speed = speed;
+    // ability
+    this.ability = ability;
+
+}
+
+// ability object
+function ability(name, type, power, accuracy, cooldown, effect, description) {
+
+}
+
+var battle = function(player, enemy, battle_menu) {
+
+    // see who goes first
+
+    // enter battle loop (continues until forfeit or one player's health <= 0)
+
+    // give xp and money
+
+}
+
+// create the player object
+var player = new robot("Player", 10, 5, 5, 5, 5, 5, 5);
+var enemy = new robot("Enemy", 10, 5, 5, 5, 5, 5, 5);
+
 // load the main menu on start up
-var player = new Player(game);
 game.state.add('main_menu', main_menu);  
 game.state.start('main_menu'); 
